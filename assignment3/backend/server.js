d1611dfa119480f885f6f42a3c67e13c4cc91766
unknown <shhivamsinghhh@gmail.com>
2023-03-09 12:58:10 +0530

    Changed title

--- assignment3/backend/server.js
+++ assignment3/backend/server.js
@@ -14,9 +14,7 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-app.get("/", (req, res) => {
-   res.send("API Running!");
-});
+
 
 app.use("/api/user",userRoutes);
 app.use("/api/chat",chatRoutes);
@@ -28,11 +26,11 @@ const __dirname1 = path.resolve();
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
   console.log(__dirname1);
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1,"../", "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
